eb396b89fa4f06d0e08603fffa4b039698fb099d
Jairo Junior <jjairojr01@gmail.com>
2020-04-13 21:57:16 -0300

    desafio concluido

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const repositoriesOptions = [
+      {
+        title: "Front-end com ReactJS",
+        techs: ["ReactJS"],
+        url: "https://github.com/Shofnip/front-end-reactjs",
+      },
+      {
+        title: "Back-end com NodeJS",
+        techs: ["NodeJS"],
+        url: "https://github.com/Shofnip/back-end-Nodejs",
+      },
+      {
+        title: "Mobile com React Native",
+        techs: ["React Native"],
+        url: "https://github.com/Shofnip/mobile-react-native",
+      },
+      {
+        title: "Desafio Web Rocketseat",
+        techs: ["NodeJS", "ReactJS"],
+        url: "https://github.com/Shofnip/desafio-web-rocketseat",
+      },
+      {
+        title: "Desafio Mobile Rocketseat",
+        techs: ["NodeJS", "React Native"],
+        url: "https://github.com/Shofnip/desafio-mobile-rocketseat",
+      },
+      {
+        title: "Calculadora Web",
+        techs: ["ReactJS"],
+        url: "https://github.com/Shofnip/calculadora-web",
+      },
+      {
+        title: "Calendário Mobile",
+        techs: ["React Native"],
+        url: "https://github.com/Shofnip/calendario-mobile",
+      },
+      {
+        title: "Foguete GoStack",
+        techs: ["NodeJS", "ReactJS", "React Native"],
+        url: "https://github.com/Shofnip/foguete-gostack",
+      },
+    ];
+
+    const newRepository =
+      repositoriesOptions[
+        Math.floor(Math.random() * repositoriesOptions.length)
+      ];
+
+    const response = await api.post("repositories", {
+      title: newRepository.title,
+      techs: newRepository.techs,
+      url: newRepository.url,
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
